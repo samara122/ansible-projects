8c5e72741791d1eec14c654d6c41b6df9affa63d
samara <saruufan@gmail.com>
2022-07-09 23:08:54 -0500

    bla

---
+++ myscripts/jenkins-install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo yum install wget -y
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+   https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo yum install jenkins java-1.8.0-openjdk-devel -y
+sudo systemctl start jenkins
+sudo systemctl enable jenkins
